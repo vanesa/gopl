61ff5b93175d5eac75ea7a77da54f69258670214
Vanesa <vanesa@gmx.net>
2017-06-23 22:58:16 -0700

    print status code

---
+++ ch1/ex1_9/main.go
@@ -0,0 +1,37 @@
+// Fetch prints the content found at a URL.
+// Excercise: Print HTTP status code
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if strings.HasPrefix(url, "http://") != true {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := io.Copy(os.Stdout, resp.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%v, \nStatus Code: %v\n", b, resp.Status)
+	}
+}
